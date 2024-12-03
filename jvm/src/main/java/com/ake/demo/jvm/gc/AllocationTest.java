59022d63f7b6e290b87c3b2b90befd3a19a3b11f
keqingyuan <695774301@qq.com>
2024-12-03 21:01:12 +0800

    fix: update add gc class

--- jvm/src/main/java/com/ake/demo/jvm/gc/AllocationTest.java
+++ jvm/src/main/java/com/ake/demo/jvm/gc/AllocationTest.java
@@ -36,5 +36,24 @@ public class AllocationTest {
      *   object space 10240K, 40% used [0x00000000fec00000,0x00000000ff000010,0x00000000ff600000)
      *  Metaspace       used 2640K, capacity 4486K, committed 4864K, reserved 1056768K
      *   class space    used 280K, capacity 386K, committed 512K, reserved 1048576K
+     *
+     *   这边如果是使用另外的一个垃圾回收器，对应的打印日志如下:
+     *
+     *    D:\Java\jdk1.8.0_291\bin\java -verbose:gc -XX:+UseParallelGC -XX:+UseParallelOldGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -Xmx20M com.ake.demo.jvm.gc.AllocationTest
+     * 2024-12-03T20:47:40.186+0800: 0.060: [GC (Allocation Failure) [PSYoungGen: 5022K->480K(6144K)] 5022K->4824K(19968K), 0.0196165 secs] [Times: user=0.02 sys=0.00, real=0.02 secs]
+     * Heap
+     *  PSYoungGen      total 6144K, used 2641K [0x00000000ff980000, 0x0000000100000000, 0x0000000100000000)
+     *  // 新生代分配了6M的空间，使用了2M的空间，剩下的应该是都直接分配到老年代去了
+     *   eden space 5632K, 38% used [0x00000000ff980000,0x00000000ffb9c410,0x00000000fff00000)
+     *   // eden的空间为5M，survivor的空间为0.5M，
+     *   from space 512K, 93% used [0x00000000fff00000,0x00000000fff78030,0x00000000fff80000)
+     *   to   space 512K, 0% used [0x00000000fff80000,0x00000000fff80000,0x0000000100000000)
+     *  ParOldGen       total 13824K, used 8440K [0x00000000fec00000, 0x00000000ff980000, 0x00000000ff980000)
+     *  // 老年代使用了13M的空间，用了8M的空间
+     *   object space 13824K, 61% used [0x00000000fec00000,0x00000000ff43e040,0x00000000ff980000)
+     *  Metaspace       used 2640K, capacity 4486K, committed 4864K, reserved 1056768K
+     *  // 元空间为什么是使用了2M的大小，它到底存放的是什么东西呢，为什么是2M？ 元空间的可用内存是4M，可以用的空间也是4M，JVM可以给元空间分配的物理内存是1056768K
+     *   class space    used 280K, capacity 386K, committed 512K, reserved 1048576K
+     *   // 这个和元空间是类似的，类空间是存放类的静态内容和运行时常量池，
      * */
 }
